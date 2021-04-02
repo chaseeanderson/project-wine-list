d87047751b240937bb02f9ec10059fc1fa75fab8
Chase Anderson <anderson.e.chase@gmail.com>
2021-04-02 10:18:56 -0700

    bug fix thank you elliot

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,13 @@ const isLoggedIn = require('../config/auth');
 router.get('/', function(req, res, next) {
   res.render('welcome', { title: 'Welcome' });
 });
-
 // GET Home (Wines) Page
 router.get('/wines/home', isLoggedIn, winesCtrl.index);
-
 // OAuth routers
 router.get('/auth/google', passport.authenticate(
   'google', 
   {scope: ['profile', 'email']}
 ));
-
 // Google OAuth login route
 router.get('/oauth2callback', passport.authenticate(
   'google',
@@ -31,6 +28,5 @@ router.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
 
 module.exports = router;
